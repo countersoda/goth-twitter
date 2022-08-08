dc5cf3f3ed21ac6a236b4d97ad2ec4f290f1b80e
Alexander Krahl <alexander.krahl@daubit.org>
2022-08-08 17:19:42 +0200

    test keeping sessions alive, add discord

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/daubit/goth"
 	"github.com/daubit/goth/gothic"
+	"github.com/daubit/goth/providers/discord"
 	"github.com/daubit/goth/providers/twitterv2"
 	"github.com/gorilla/pat"
 	"github.com/joho/godotenv"
@@ -33,10 +34,12 @@ func main() {
 		//twitterv2.New(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"), "http://127.0.0.1:3000/auth/twitterv2/callback"),
 		// If you'd like to use authenticate instead of authorize in Twitter provider, use this instead.
 		twitterv2.NewAuthenticate(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"), "http://127.0.0.1:3000/auth/twitterv2/callback"),
+		discord.New(os.Getenv("DISCORD_KEY"), os.Getenv("DISCORD_SECRET"), "http://127.0.0.1:3000/auth/discord/callback", discord.ScopeIdentify, discord.ScopeEmail, discord.ScopeGuilds),
 	)
 
 	m := make(map[string]string)
 	m["twitterv2"] = "Twitter"
+	m["discord"] = "Discord"
 
 	var keys []string
 	for k := range m {
@@ -58,6 +61,14 @@ func main() {
 		t.Execute(res, user)
 	})
 
+	p.Get("/test/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		providerName, _ := gothic.GetProviderName(req)
+		provider, _ := goth.GetProvider(providerName)
+		value, _ := gothic.GetFromSession(providerName, req)
+		sess, _ := provider.UnmarshalSession(value)
+		sess.GetAuthURL()
+	})
+
 	p.Get("/logout/{provider}", func(res http.ResponseWriter, req *http.Request) {
 		gothic.Logout(res, req)
 		res.Header().Set("Location", "/")
@@ -75,10 +86,17 @@ func main() {
 	})
 
 	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
-		t, _ := template.ParseFiles("templates/index.html")
+		//t, _ := template.ParseFiles("templates/index.html")
+		//t.Execute(res, providerIndex)
+		t, _ := template.New("foo").Parse(indexTemplate)
 		t.Execute(res, providerIndex)
+
 	})
 
 	log.Println("listening on localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", p))
 }
+
+var indexTemplate = `{{range $key,$value:=.Providers}}
+    <p><a href="/auth/{{$value}}">Log in with {{index $.ProvidersMap $value}}</a></p>
+{{end}}`
